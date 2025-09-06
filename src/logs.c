b133728a5e5e598ac54ad45afdc95ae4c55bdaa7
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-09-06 16:59:58 -0400

    refactor(logs): use pthread instead
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- src/logs.c
+++ src/logs.c
@@ -2,19 +2,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <threads.h>
+#include <pthread.h>
 
-static mtx_t print_mtx;
-static void print_mtx_init(void) {
-    mtx_init(&print_mtx, mtx_plain | mtx_recursive);
-}
+static pthread_mutex_t print_mtx = PTHREAD_MUTEX_INITIALIZER;
 int debug_log(enum DebugLogType type, const char *fmt, ...) {
-    static once_flag init_flag = ONCE_FLAG_INIT;
-    call_once(&init_flag, print_mtx_init);
     va_list va;
     // this *might* be due to C23.
     va_start(va, fmt);
-    mtx_lock(&print_mtx);
+    pthread_mutex_lock(&print_mtx);
     switch (type) {
     case INFO:
         printf("\033[34;1m[INFO]\033[0m: ");
@@ -27,6 +22,6 @@ int debug_log(enum DebugLogType type, const char *fmt, ...) {
         break;
     }
     auto ret = vprintf(fmt, va);
-    mtx_unlock(&print_mtx);
+    pthread_mutex_unlock(&print_mtx);
     return ret;
 }
