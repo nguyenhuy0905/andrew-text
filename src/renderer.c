621a9f809163f27343e7568f6cf6a88dce38e4fc
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-09-11 16:50:51 -0400

    wip: at least i could render static text now
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- src/renderer.c
+++ src/renderer.c
@@ -1,19 +1,28 @@
+#include "logs.h"
 #include "renderer.h"
 #include <raylib.h>
 #include <math.h>
+#include <stdio.h>
 
 void render_file(struct StrSlice t_filename) {
+    // size_t text_pos = 0;
+    char buffer[1024];
+    // let's just get the file onto the screen
+    auto file = fopen(t_filename.buf, "r");
+    if(file == nullptr) {
+        debug_log(ERROR, "Path %s cannot be opened\n", t_filename.buf);
+        return;
+    }
     InitWindow(800, 500, t_filename.buf);
     SetTargetFPS(60);
-    Rectangle rec = {.x = 0, .y = 0, .width = 60, .height = 60};
+    auto font = LoadFont("./assets/IosevkaNerdFontMono-Regular.ttf");
+    fread(buffer, 1, sizeof(buffer), file) == sizeof(buffer));
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RED);
-        DrawRectangleRec(rec, LIGHTGRAY);
-        rec.x = fmodf((float)(rec.x + 8.0), 800.0);
-        rec.y = fmodf((float)(rec.y + 5.0), 500.0);
-        DrawText(t_filename.buf, 300, 200, 28, BEIGE);
+        ClearBackground(WHITE);
+        DrawTextEx(font, buffer, (Vector2){.x = 10, .y = 10}, 16, 1, BLACK);
         EndDrawing();
     }
+    UnloadFont(font);
     CloseWindow();
 }
